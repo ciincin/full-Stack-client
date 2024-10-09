968a8e04212fefb9952c050846e7236c9dab20c8
Cinthya <cinthyaredondo12@gmail.com>
2024-10-09 19:13:27 +0200

    I select and use a API to get the recipes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,21 +2,21 @@ import "./Footer.css";
 
 function Footer() {
   return (
-    <div className="footer-wrapper">
-      <p>2024 - Cinthya Redondo Soto. All rights reserved.</p>
-      <div className="contact-wrapper">
-          <a className="social" href="https://www.linkedin.com/in/cinthyars/">LinkedIn</a>
-          <a className="social" href="https://github.com/ciincin">Github</a>
-      </div>
-      <p>
-        This website is intended solely for educational purposes. It is a
-        project created by Cinthya Redondo Soto, full-stack developer to
-        practice and demonstrate her programming skills. I do not seek to
-        generate any revenue or financial gain from this website. Any
-        resemblance to real businesses, services, or products is purely
-        coincidental.
-      </p>
-    </div>
+    <footer>
+      <div>2024 - Cinthya Redondo Soto. All rights reserved.</div>
+      <a className="social" href="https://www.linkedin.com/in/cinthyars/">
+        <img
+          src="src/assets/socialMedia/linkedin.png"
+          alt="https://www.flaticon.es/iconos-gratis/github - Github iconos creados por Pixel perfect - Flaticon"
+        />
+      </a>
+      <a className="social" href="https://github.com/ciincin">
+        <img
+          src="src/assets/socialMedia/github.png"
+          alt="https://www.flaticon.es/iconos-gratis/github - Github iconos creados por Pixel perfect - Flaticon"
+        />
+      </a>
+    </footer>
   );
 }
 
---
+++ src/components/useFetchRecipes/useFetchRecipes.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+function useFetchRecipes() {
+
+    const recipeApi = 'https://dummyjson.com/recipes'
+
+    const [recipes, setRecipes]= useState([])
+    const [errorRecipes,setErrorRecipes] = useState(null)
+    const [loadingRecipes, setLoadingRecipes] = useState(true)
+
+    useEffect(()=>{
+        async function fetchRecipes (){
+            try{
+                const response = await fetch(recipeApi)
+                if(response.ok){
+                    const data = await response.json()
+                    
+                    setRecipes(data.recipes)
+                } else {
+                    const dataError = await response.json()
+                    setErrorRecipes(`data not found ${dataError.msg}`)
+                }
+            } catch(error){
+                setErrorRecipes(`Error fetching recipes info: ${error.message}`)
+            }finally{
+                setLoadingRecipes(false)
+            }
+        }
+        fetchRecipes()
+    },[])
+
+  return {recipes, errorRecipes, loadingRecipes}
+}
+
+export default useFetchRecipes
+
